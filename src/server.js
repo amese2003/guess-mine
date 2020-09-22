1450aded645e6b3e9d5b61df110f02ab5920fd83
Yong Ju Kwak <amese1225@gmail.com>
2020-09-22 15:12:57 +0900

    #2.1 Hello World part Two

--- src/server.js
+++ src/server.js
@@ -19,8 +19,7 @@ const handleListening = () => {
 const server = app.listen(PORT, handleListening);
 const io = socketIO.listen(server)
 
-let sockets = []
-
 io.on("connection", (socket) => {
-    sockets.push(socket.id)
+    //setTimeout(() => socket.broadcast.emit("hello"), 5000);
+    socket.on("helloGuys", () => console.log("the client said hello"));
 });
--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,7 @@
-io("/");
+const socket = io("/");
+
+socket.on("hello", () => {
+    console.log("somebody joined")
+});
+
+setTimeout(() => socket.emit("helloGuys"), 4000);
