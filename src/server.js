197f7128f390a0f98443d7d5e41170663388abde
Yong Ju Kwak <amese1225@gmail.com>
2020-09-23 17:20:48 +0900

    2.3 Chat with SocketIO

--- src/server.js
+++ src/server.js
@@ -21,5 +21,14 @@ const io = socketIO.listen(server)
 
 io.on("connection", (socket) => {
     //setTimeout(() => socket.broadcast.emit("hello"), 5000);
-    socket.on("helloGuys", () => console.log("the client said hello"));
+    socket.on("newMessage", ({ message }) => {
+        socket.broadcast.emit("messageNotif", { 
+            message,
+            nickname:socket.nickname || "Anon"
+        });
+    });
+
+    socket.on("setNickname", ({nickname}) => {
+        socket.nickname = nickname;
+    });
 });
